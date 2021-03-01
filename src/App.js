be3611232fbd54f48737c38cae6fcc99e32d8879
aripan <aripan017@gmail.com>
2021-03-01 16:22:18 +0100

    Worked on bonus part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
+import IndividualPlayer from "./components/IndividualPlayer/IndividualPlayer";
+import playerInformation from "./fake data/fakeData.json";
+import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
+import { Row, Col, Container, ListGroup, Card } from "react-bootstrap";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [teamPlayers, setTeamPlayers] = useState([]);
+
+  const handleAddPlayer = (player) => {
+    const newList = [...teamPlayers, player];
+    setTeamPlayers(newList);
+    console.log(newList);
+  };
+
+  useEffect(() => {
+    setPlayers(playerInformation);
+  }, []);
+
+  const totalBudget = teamPlayers.reduce(
+    (total, player) => total + player.Auction,
+    0
+  );
+
   return (
     <div className="App">
-      <Header></Header>
-      <Main></Main>
+      <Header className="header"></Header>
+      <div className="players-container">
+        <h2 style={{ textAlign: "center" }}>Players</h2>
+        <Container>
+          <Row>
+            {players.map((player) => (
+              <Col sm={12} md={6} lg={4} key={player.id}>
+                <IndividualPlayer
+                  player={player}
+                  handleAddPlayer={handleAddPlayer}
+                ></IndividualPlayer>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+      <div className="team-container">
+        <h2 style={{ textAlign: "center" }}>Selected Team</h2>
+        <Container>
+          <Card style={{ width: "16rem" }}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h6>Number of Players: {teamPlayers.length}</h6>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <h6>Players name & auction price</h6>
+                {teamPlayers.map((player) => (
+                  <SelectedPlayers
+                    key={player.id}
+                    player={player}
+                  ></SelectedPlayers>
+                ))}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <h6>Total Budget: ${totalBudget}</h6>{" "}
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Container>
+      </div>
     </div>
   );
 }
--- src/components/IndividualPlayer/IndividualPlayer.js
+++ src/components/IndividualPlayer/IndividualPlayer.js
@@ -1,34 +1,33 @@
 import React from "react";
+import { Card, Button, ListGroup } from "react-bootstrap";
 import "./IndividualPlayer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 const IndividualPlayer = (props) => {
   const { image, name, Born, Nationality, Role, Auction } = props.player;
 
   return (
-    <div className="individual-player">
-      <div className="individual-player-intro">
-        <img style={{ width: "150px", height: "100px" }} src={image} alt="" />
-        <h3 style={{ paddingLeft: "15px" }}>{name}</h3>
-      </div>
-      <div className="individual-player-detail-info">
-        <h4>Personal Information</h4>
-        <p>
-          <strong>Born:</strong> <em>{Born}</em>
-        </p>
-        <p>
-          <strong>Nationality:</strong> <em>{Nationality}</em>
-        </p>
-        <p>
-          <strong>Role:</strong> <em>{Role}</em>
-        </p>
-        <p>
-          <strong>Auction Price:</strong> <em>${Auction}</em>
-        </p>
-      </div>
-      <button onClick={() => props.handleAddPlayer(props.player)}>
-        Add to Team
-      </button>
-    </div>
+    <Card style={{ width: "16rem", margin: "1rem" }}>
+      <Card.Img
+        variant="top"
+        src={image}
+        style={{ width: "100%", height: "150px" }}
+      />
+      <Card.Header style={{ fontWeight: "700" }}>{name}</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>Born: {Born}</ListGroup.Item>
+        <ListGroup.Item>Nationality: {Nationality}</ListGroup.Item>
+        <ListGroup.Item>Role: {Role}</ListGroup.Item>
+        <ListGroup.Item>Auction Price: ${Auction}</ListGroup.Item>
+      </ListGroup>
+      <Button
+        variant="primary"
+        onClick={() => props.handleAddPlayer(props.player)}
+      >
+        <FontAwesomeIcon icon={faUserPlus} /> Add To Team
+      </Button>
+    </Card>
   );
 };
 
