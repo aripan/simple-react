033175f5ee1146184b2bb68c5967a3f0e76f0f09
aripan <aripan017@gmail.com>
2021-03-01 12:05:02 +0100

    Pass data to Players component

--- src/components/IndividualPlayer/IndividualPlayer.js
+++ src/components/IndividualPlayer/IndividualPlayer.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./IndividualPlayer.css";
 
-const IndividualPlayer = () => {
+const IndividualPlayer = (props) => {
+  const { image, name, Born, Nationality, Role, Auction } = props.player;
+
   return (
     <div className="individual-player">
-      <p>each players come here</p>
+      <div className="individual-player-intro">
+        <img style={{ width: "150px" }} src={image} alt="" />
+        <h3 style={{ paddingLeft: "15px" }}>{name}</h3>
+      </div>
+      <div className="individual-player-detail-info">
+        <h4>Personal Information</h4>
+        <p>
+          <strong>Born:</strong> <em>{Born}</em>
+        </p>
+        <p>
+          <strong>Nationality:</strong> <em>{Nationality}</em>
+        </p>
+        <p>
+          <strong>Role:</strong> <em>{Role}</em>
+        </p>
+        <p>
+          <strong>Auction Price:</strong> <em>{Auction}</em>
+        </p>
+      </div>
+      <button onClick={props.handleAddPlayer}>Add to Team</button>
     </div>
   );
 };
--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IndividualPlayer from "../IndividualPlayer/IndividualPlayer";
 import "./Players.css";
+import playerInformation from "../../fake data/fakeData.json";
 
 const Players = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    setPlayers(playerInformation);
+  }, []);
+
+  const handleAddPlayer = () => {
+    console.log("working");
+  };
+
   return (
     <div>
       <h3>Players</h3>
       <div className="players">
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
-        <IndividualPlayer></IndividualPlayer>
+        {players.map((player) => (
+          <IndividualPlayer
+            player={player}
+            key={player.id}
+            handleAddPlayer={handleAddPlayer}
+          ></IndividualPlayer>
+        ))}
       </div>
     </div>
   );
